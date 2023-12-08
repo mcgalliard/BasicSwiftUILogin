2905d54a9519e7bea307cb644d55887552bdcbda
Chris McGalliard <65915981+mcgalliard@users.noreply.github.com>
2023-12-07 17:58:38 -0800

    Initial Commit

---
+++ TestingDecember2024/TestingDecember2024App.swift
@@ -0,0 +1,17 @@
+//
+//  TestingDecember2024App.swift
+//  TestingDecember2024
+//
+//  Created by Chris McGalliard on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingDecember2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
